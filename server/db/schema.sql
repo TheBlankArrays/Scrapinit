30f7ca65e44980ddf1444dba38f463738db2f329
Dominika <bienkowska.dominika@gmail.com>
2015-06-19 08:54:41 -0700

    [feature] database: adds schema for sql

---
+++ server/db/schema.sql
@@ -0,0 +1,31 @@
+
+USE scraping;
+
+DROP TABLE IF EXISTS `user`
+
+CREATE TABLE `user`(
+    `userId` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `scrapes` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`userId`)
+);
+
+ALTER TABLE `User` ADD CONSTRAINT `User_fk0` FOREIGN KEY (`scrapes`) REFERENCES `scrape`(`url`);
+
+DROP TABLE IF EXISTS `scrapes`;
+
+CREATE TABLE `scrapes` (
+    `urlID` INT NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`urlID`)
+);
+
+DROP TABLE IF EXISTS `url`;
+
+CREATE TABLE `url` (
+    `URLID` INT NOT NULL AUTO_INCREMENT,
+    `UserID` INT NOT NULL,
+    `scrapesID` INT NOT NULL,
+    PRIMARY KEY (`URLID`)
+);
