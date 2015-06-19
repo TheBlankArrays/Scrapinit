e236b6a6da319d8b0b1a4540148d0fd49914f007
John Murphy <chiefsmurph@gmail.com>
2015-06-19 16:43:51 -0700

    [Refactor] Removed state.go -> results in homeController

--- client/app/home/homeController.js
+++ client/app/home/homeController.js
@@ -1,6 +1,5 @@
 angular.module('app.home', ['app.home.addUrl', 'app.home.results', 'ui.router'])
 .controller('homeController', function ($scope, $state) {
-  $state.go('home.results');
   $scope.html= '';
   $scope.add = function() {
     $scope.html = 'newValues';
