ac298579f75b004a62834fd256867a55e8d2e76a
Nayigiziki <josephnayigiziki@gmail.com>
2015-06-20 13:56:01 -0700

    [fix] server routes

--- server/routes.js
+++ server/routes.js
@@ -3,16 +3,15 @@ var authController = require('./controllers/authController');
 var urlController = require('./controllers/urlController');
 
 var setup = function(app) {
-	app.route('/api/users/login')
-    .post(authController.login);
+  app.route('/api/users/login')
+  .post(authController.login);
   app.route('/api/users/signup')
-    .post(authController.signup)
-    .get(authController.login);
-
-	app.route('/api/users/addUrl')
-    .post(function(req, res, next) {
-			console.log(req.body.url);
-			res.send("you are crazy watching this url here: " + req.body.url);
-		});
-}
+  .post(authController.signup)
+  .get(authController.login);
+  app.route('/user/urls')
+  .get(urlController.getUrls)
+  .post(urlController.postUrl); 
+  app.route('/user/url')
+  .post(urlController.getExternalUrl);
+};
 module.exports.setup = setup;
