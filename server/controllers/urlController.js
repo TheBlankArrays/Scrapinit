f260225d09b73e9d10104e0461d2167c725dc7db
Nayigiziki <josephnayigiziki@gmail.com>
2015-06-22 16:18:36 -0700

    [Feature] added the ability to add urls to the db & getUrls route correctly routes to the getURls function

--- server/controllers/urlController.js
+++ server/controllers/urlController.js
@@ -8,74 +8,76 @@ module.exports = {
   getUrls: function (req, res, next) {
     console.log('get Urls');
 
-  //get the current user
-    //user DNE --> send to login page
-  //get the user's urls
-    //if null, send empty array
-    //send array of urls  
+//Testing response
+      res.status(201).json({
+        urls : [{
+          url : 'joedaddy.com',
+            whatToWatch : 'thePrize',
+            pingRate : '5 min',
+            active : 'yes'
+          }]
+      });
+
+
+
+      // db.User.getUrls(user, function(urls){
+      //   res.status(201).json({
+      //     urls : [{
+      //       url : 'joedaddy.com',
+      //       whatToWatch : 'thePrize',
+      //       pingRate : '5 min',
+      //       active : 'yes'
+      //     }]
+      //   });
+      // })
+      // .catch(function (err) {
+      //   res.status(403).json({message: err.message});
+      // });
   },
-  addUrl: function (req, res, next) {
-    console.log('post Urls');
+  addUrl: function (req, res, next, cb) {
     var email = req.session.email;
-    console.log('email - '+ email);
-    // var url = req.url;
+    var url = req.body;
 
-    // db.User.findOne({
-    //   where: {
-    //     email: user.email
-    //   }
-    // })
-    // .then(function (userFound) {
-    //   if (userFound) {
-    //     db.Url.findOne({
-    //       where: {}
-    //     })
-
-
-    //     // userFound.comparePasswords(user.password, function (result) {
-    //     //   if (result) {
-    //     //     req.session.email = userFound.email;
-    //     //     res.status(200).json(userFound);
-    //     //   }else{
-    //     //     res.status(401).json({error: 'User or Password invalid'});
-    //     //   }
-    //     // });
-
-
-    //   } else {
-        
-    //     res.status(401).json({error: 'Error - user does not exist'});
-    //   }
-    // });
-
-
-//get the url, html & selector
-  //check if the values exist
-  //if does not exist
-  //return an request to re-attempt the request
-//get the current user
-  //user DNE --> send to login page
-//create a new url
-  //return an request to re-attempt the request
-//add the url to the user
-  //return a request to re-attempt the request
-//send the updated urls to the client
-  },
-  modifyUrl: function(req, res, next){
-
-  },
-
-  getExternalUrl: function(req,res, next){
-    var url = req.body.url;
-    console.log(req.body.url);
-    basicScraper.get(url, function(error, response, html){
-      if(!error && response.statusCode === 200){
-        console.log('success');
-        res.send(html);
-      } else {
-        console.log('failure');
-        res.send("error no response");
+    db.User.findOne({
+      where: {
+        email: email
+      }
+    })
+    .then(function (userFound) {
+      if (userFound) {
+        db.Url.findOne({
+          where: url
+        })
+        .then(function(urlFound) {
+          if(urlFound){
+            // db.associate(userFound.email, urlFound.url, {html: html, selector: selector})//need to store and send the html & selector
+            cb('url found');
+          } else {
+            db.Url.create(url)
+            .then(function (newUrl){
+                // db.associate(userFound.email, newUrl.url, {html: html, selector: selector})//need to store and send the html & selector
+              cb('url created');
+            })
+            .catch(function (err) {
+              res.status(403).json({message: err.message});
+            });
+          }
+        });
       }
     });
-  }
+
+},
+getExternalUrl: function(req,res, next){
+  var url = req.body.url;
+  console.log(req.body.url);
+  basicScraper.get(url, function(error, response, html){
+    if(!error && response.statusCode === 200){
+      console.log('success');
+      res.send(html);
+    } else {
+      console.log('failure');
+      res.send("error no response");
+    }
+  });
+}
 };
