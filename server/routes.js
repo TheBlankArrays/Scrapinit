2a42670e6c2936943a8a67798886082f504ba12f
Dominika <bienkowska.dominika@gmail.com>
2015-06-22 16:59:58 -0700

    [Fix] resolved merge conflict

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 //add Controllers to handle the routes
 var authController = require('./controllers/authController');
 var urlController = require('./controllers/urlController');
+var webshot = require('webshot');
 
 var setup = function(app) {
   app.route('/api/users/login')
@@ -28,9 +29,8 @@ var setup = function(app) {
 	app.route('/api/users/addUrl')
     .post(function(req, res, next) {
 			console.log(req.body.url);
-			var webshot = require('webshot');
+		
 			var urlWithoutHTTP = req.body.url.substr(7);
-
 			webshot(req.body.url, '../client/assets/' + urlWithoutHTTP + '.jpg', function(err) {
 				// screenshot now saved to google.png// screenshot now saved to hello_world.png
 				res.send('assets/' + urlWithoutHTTP + '.jpg');
