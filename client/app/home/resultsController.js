1451941ed4f16c431f2e09b1c26165c4302bb54a
Antonio Pol <pol370@gmail.com>
2015-06-22 15:18:55 -0700

    [Feature] Results Controller request to list of URL

--- client/app/home/resultsController.js
+++ client/app/home/resultsController.js
@@ -1,4 +1,34 @@
 angular.module('app.home.results', [])
-.controller('resultsController', function ($scope) {
+.controller('resultsController', function ($scope, Url) {
+
+  $scope.getUrls = function () {
+    Url.getUrls(function (err, urls) {
+      if (err) {
+        $scope.error = 'We can´t retrieve the URLS';
+      }else {
+        $scope.setUrls(urls);
+      }
+    });
+  };
+
+})
+.factory('Url', function ($http) {
+
+  var getUrls = function (callback) {
+    $http({
+      method: 'GET',
+      url: '/api/users/urls'
+    })
+    .success(function(urls) {
+      callback(false, urls);
+    })
+    .error(function(err) {
+      callback(true);
+    });
+  };
+
+  return {
+    getUrls: getUrls
+  }
 
 });
