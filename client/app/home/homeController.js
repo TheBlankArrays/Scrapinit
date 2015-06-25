5773553d13099845b925d42572720532277e201d
Nayigiziki <josephnayigiziki@gmail.com>
2015-06-25 15:58:34 -0700

    [Fix] Added client retrieval of urls from server

--- client/app/home/homeController.js
+++ client/app/home/homeController.js
@@ -1,30 +1,25 @@
-angular.module('app.home', ['app.home.addUrl', 'app.home.results', 'ui.router'])
+angular.module('app.home', ['app.home.addUrl', 'app.home.results', 'ui.router', ])
 .controller('homeController', function ($scope, $state, $http) {
 
-   $scope.html = '';
+
    $scope.url = 'http://';
    $scope.urls = [];
    $scope.loading = false;
    console.log($scope.urls);
 
-   // load current urls
-   $http.get('/api/users/list')
-    .success(function(data) {
-      var urls = data.urls;
-      for (var i = 0; i < urls.length; i++) {
-        $scope.urls.push({url: urls[i].url, img: urls[i].UserUrl.cropImage});
-      }
-    });
-
-
-   // request current users' urls from server right now on init
 
    $scope.logout = function() {
      $http.get("/api/users/logout")
        .success(function (data) {
          $state.go('login');
        });
-   }
+   };
+
+
+  $scope.setUrls = function(urlsObject){
+    $scope.urls = urlsObject;
+  
+  };
 
    $scope.add = function() {
 
@@ -71,5 +66,6 @@ angular.module('app.home', ['app.home.addUrl', 'app.home.results', 'ui.router'])
 
          //   });
    };
-
+   console.log('going to results');
+  $state.go('home.results');
 });
--- client/app/home/resultsController.js
+++ client/app/home/resultsController.js
@@ -1,7 +1,10 @@
 angular.module('app.home.results', [])
 .controller('resultsController', function ($scope, Url) {
+  console.log('app home results');
+
 
   $scope.getUrls = function () {
+    console.log('in home results');
     Url.getUrls(function (err, urls) {
       if (err) {
         $scope.error = 'We can´t retrieve the URLS';
@@ -11,15 +14,21 @@ angular.module('app.home.results', [])
     });
   };
 
+  $scope.getUrls();
 })
 .factory('Url', function ($http) {
 
   var getUrls = function (callback) {
     $http({
       method: 'GET',
-      url: '/api/users/urls'
+      url: '/api/users/list'
     })
-    .success(function(urls) {
+    .success(function(data) {
+      var urls = [];
+      var urlArray = data.urls;
+      for (var i = 0; i < urlArray.length; i++) {
+        $scope.urls.push({url: urlArray[i].url, img: urlArray[i].UserUrl.cropImage});
+      }
       callback(false, urls);
     })
     .error(function(err) {
