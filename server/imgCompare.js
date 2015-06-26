be32ba491a2bcc8d749b89662df191b04aa25b9a
Dominika <bienkowska.dominika@gmail.com>
2015-06-26 14:41:23 -0700

    [Feature] cleaned up imgCompare

--- server/imgCompare.js
+++ server/imgCompare.js
@@ -4,25 +4,19 @@ var path = require('path');
 module.exports = {
 
   compare: function (image1, image2) {
-    // var img1 = './client/assets/qvNot4g0dBZNio45GtwpXfopy8poLxm8/google.com.jpg';
-    // var img2 = './client/assets/qvNot4g0dBZNio45GtwpXfopy8poLxm8/google.com.jpg';
-    // var img3 = './client/assets/BbRKwbk69wJmh8YW-G5QkdNquxJ_6EyB/google.com.jpg';       
-    console.log('image1', image1)
-    console.log('__dir', __dirname)
+     
+
     var newDirName = __dirname + '../';
     var newDir = __dirname.slice(0,-6);
-    console.log('newDir',newDir)
-    var a1 = path.join(newDir,'client', image1);
-    var a2 = path.join(newDir,'client', image2);
-    console.log('a1', a1)
-    console.log('a2', a2)
+    var img1 = path.join(newDir,'client', image1);
+    var img2 = path.join(newDir,'client', image2);
+    console.log('image1:', img1)
+    console.log('image2:', img2)
 
 
-// a1= '/Users/dominikabienkowska/Desktop/Scrapinit/client/assets/-0cvRcQCJRzYSJPBvytR5y4zVIPjTPRl/google.com.jpg';
-// a2 ='/Users/dominikabienkowska/Desktop/Scrapinit/client/assets/1/google.comcompare.jpg';
     imageDiff({
-      actualImage: a1,
-      expectedImage: a2,
+      actualImage: img1,
+      expectedImage: img2,
       diffImage: 'difference.png',
     }, function (err, imagesAreSame) {
       console.log('IMAGE DIFF : images are the same: ',imagesAreSame)
