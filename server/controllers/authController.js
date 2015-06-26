584ff0da0135b137b7e1edfdba36384847dab683
John Murphy <chiefsmurph@gmail.com>
2015-06-26 15:48:49 -0700

    [style] comments out html directive in result

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,8 +15,8 @@ module.exports = {
       if (userFound) {
         userFound.comparePasswords(user.password, function (result) {
           if (result) {
-            req.session.email = result.email;
-            req.session.user_id = result.id;
+            req.session.email = userFound.email;
+            req.session.user_id = userFound.id;
             res.status(200).json(userFound);
           }else{
             res.status(400).json({error: 'User or Password invalid'});
