c8a9593c47e39118833a09ff15b06792df932657
Chris Grovers <chrisgrovers@gmail.com>
2015-06-29 12:26:27 -0700

    [Refactor] updated addCron to use CronJobManager. Added functions to stop a cronjob, and delete a cronjob

--- server/controllers/CronController.js
+++ server/controllers/CronController.js
@@ -1,7 +1,7 @@
 var basicScraper = require('./basicScraperController');
 var getExternalUrl = require('./urlController').getExternalUrl;
 var CronJob = require('cron').CronJob;
-var CronJobManager = require('crontab_manager');
+var CronJobManager = require('cron-job-manager');
 var secret = require('../../config.js');
 var db = require('../db.js');
 var Sequelize = require('sequelize');
@@ -13,24 +13,92 @@ var transporter = nodemailer.createTransport({
 });
 
 
-// var CronJob = require('cron').CronJob;
+var manager = new CronJobManager();
 
 module.exports = {
   addCron: function(UserUrl, url) {
+    console.log('we got some jobs', manager.jobs);
     var userUrl = UserUrl;
+    var key = UserUrl.url_id.toString() + UserUrl.user_id.toString();
+    console.log('Starting cronJob', key);
+    var action = UserUrl.compare || 'image';
 
     // minutes
-    var freq = '* */' + UserUrl.frequency + ' * * * *';
+    // var freq = '* */' + UserUrl.frequency + ' * * * *';
 
     // TEST every 10 seconds
-    // var freq = '*/10 * * * * *';
+    var freq = '*/10 * * * * *';
 
     // seconds
     // var freq = '*/' + UserUrl.frequency + ' * * * * *';
-    
-    var job = new CronJob({
-      cronTime: freq,
-      onTick: function() {
+
+    manager.add(key, freq, function() {
+        console.log('checking', url, 'for changes');
+         var oldImg = UserUrl.cropImage;
+         var email = UserUrl.email;
+         var website = url
+         var params = {
+          h: UserUrl.cropHeight,
+          w: UserUrl.cropWidth,
+          x: UserUrl.cropOriginX,
+          y: UserUrl.cropOriginY
+        }
+
+        // get the server to render the page with params coordinates
+        basicScraper.getScreenshot(website, UserUrl.user_id, function(img1, email) {
+          basicScraper.cropImg(img1, params, true, function(newImg) {
+            console.log('old image path', oldImg);
+            console.log('new image path', newImg);
+
+
+            compare(oldImg, newImg, function (equal){
+
+              if (!equal){
+
+                console.log('change detected on', website, 'sending email to ', email);
+
+                var mailOptions = {
+                    from: "The Blank Arrays <postmaster@sandbox72a87403dd654630bfa3c4b021cda08d.mailgun.org>", // sender address
+                    // currently accessing only one user email
+                    to: email, // list of receivers
+                    subject: 'We found some tubular changes!', // Subject line
+                    text: 'Hi there! It looks like we found a change on '+ website + '!', // plaintext body
+                    html: "<span>The Scrapinit team found a change on " + website +"!</span>",
+                        // html: '<b>Hello world </b>' // html body
+                };
+
+                // Send email function
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        console.log('Message sent: ' + info.response);
+                    } //  else statemenet  
+                }); //  transporter.sendMail(mailOptions, function(error, info){
+              }; // if (!equal){
+            }) // compare(img1, img2, function (equal){
+          }); // basicScraper.cropImg(img1, params, true, function(img2) {
+        }, email); // basicScraper.getScreenshot()
+      })
+    manager.start(key);
+  },
+
+  stopCron: function(UserUrl) {
+    var key = UserUrl.url_id.toString() + UserUrl.user_id.toString();
+    console.log('Stopping cronJob', key);
+    manager.stop(key);
+  },
+
+  deleteCron: function(userUrl) {
+    var key = UserUrl.url_id.toString() + UserUrl.user_id.toString();
+    console.log('Deleting cronJob', key);
+    manager.stop(key);
+  },
+
+}
+
+var compareFunc = {
+  image: function(UserUrl, url) {
         console.log('checking', url, 'for changes');
          var oldImg = UserUrl.cropImage;
          var email = UserUrl.email;
@@ -77,12 +145,5 @@ module.exports = {
             }) // compare(img1, img2, function (equal){
           }); // basicScraper.cropImg(img1, params, true, function(img2) {
         }, email); // basicScraper.getScreenshot()
-      },
-      start: false,
-      timeZone: "America/Los_Angeles"
-    });
-    job.start();
-  }
+      }
 }
-
-
