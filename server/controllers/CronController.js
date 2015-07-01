aec8038d8a1e65d80191f9552f934524f12f41e3
Chris Grovers <chrisgrovers@gmail.com>
2015-07-01 15:58:04 -0700

    [Fix] fixed in getScreenshot function. Now adds path and email to callback as parameters

--- server/controllers/CronController.js
+++ server/controllers/CronController.js
@@ -36,7 +36,7 @@ module.exports = {
       }; // or (var i = 0; i < allUsers.length; i++){
     }); // .then(function(allUsers) {
   },
-
+  
   addCron: function(UserUrl, url) {
     UserUrl.status = true;
     var userUrl = UserUrl;
@@ -69,7 +69,14 @@ module.exports = {
           x: UserUrl.cropOriginX,
           y: UserUrl.cropOriginY
         };
+
+        // TODO: 
+        // check userUrl if comparing screenshot or ocr values?
+        // if (UserUrl.) {
+          // compareUtils.compareOCR(value, value, value);
+        // }
         // compares screenshot, sends email if there is a difference in image
+
         compareUtils.compareScreenShot(UserUrl, url, email, params, oldImg);
       } else {
         manager.stop()
@@ -77,8 +84,7 @@ module.exports = {
     });
     manager.start(key);
   },
-  updateCron: function() {
-  },
+
   startCron: function(user_id, url_id) {
     var key = url_id.toString() + user_id.toString();
     if (manager.exists(key)) {
@@ -88,11 +94,13 @@ module.exports = {
       console.log('error, cronjob', key, ' does not exist');
     }
   },
+
   stopCron: function(user_id, url_id) {
     var key = url_id.toString() + user_id.toString();
     console.log('Stopping cronJob', key);
     manager.stop(key);
   },
+
   deleteCron: function(user_id, url_id) {
     var key = url_id.toString() + user_id.toString();
     console.log('Deleting cronJob', key);
@@ -101,30 +109,34 @@ module.exports = {
 };
 
 var compareUtils = {
+  compareOCR: function() {
+    // TODO: take values that are input to it, pass it through compare ocr functions? Should be in basicScroperController?
+  },
 
   compareScreenShot: function(UserUrl, website, email, params, oldImg) {
     // gets screenshot of website
-    basicScraper.getScreenshot(website, UserUrl.user_id, function(status, path) {
+    basicScraper.getScreenshot(website, UserUrl.user_id, function(img1, email) {
       // crops new image so we can compare the the old cropped image
-
-      basicScraper.cropImg(path, params, true, function(newImg) {
+      basicScraper.cropImg(img1, params, true, function(newImg) {
         // checks for difference in pictures
         compare(oldImg, newImg, function (equal){
+          console.log('I have access to email inside of compare', email);
           if (!equal){
             // set status to false since we are stopping the cronjob
             UserUrl.status = false;
             // stop cronjob
             module.exports.stopCron(UserUrl.user_id, UserUrl.url_id)
             // if images are not equal, send an email
-            compareUtils.sendEmail(website, email);
+            compareUtils.sendEmail(website, email, oldImg, newImg);
           }; // if (!equal){
         }); // compare(img1, img2, function (equal){
       }); // basicScraper.cropImg(img1, params, true, function(img2) {
     }, email); // basicScraper.getScreenshot()
-  },
+  }, // compareScreenShot: function(UserUrl, website, email, params, oldImg) {
 
-  sendEmail: function(website, email) {
+  sendEmail: function(website, email, oldImg, newImg) {
     console.log('change detected on', website, 'sending email to ', email);
+    console.log('sendEmail I have access to image paths', oldImg, newImg)
     // parameters for email
     var mailOptions = {
         from: "The Blank Arrays <postmaster@sandbox72a87403dd654630bfa3c4b021cda08d.mailgun.org>", // sender address
@@ -132,15 +144,24 @@ var compareUtils = {
         subject: 'We found some tubular changes!', // Subject line
         text: 'Hi there! It looks like we found a change on '+ website + '!', // plaintext body
         html: "<span>The Scrapinit team found a change on " + website +"!</span>",
+        attachments: [
+        {
+          path: oldImg
+        },
+        {
+          path: newImg
+        }
+        ]
     };
     // Send email function
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-          console.log(error);
-        } else {
-          console.log('Message sent: ' + info.response);            
-        }; // else statement  
-    }); // transporter.sendMail(mailOptions, function(error, info){
-  }
-
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     if(error){
+    //       console.log(error);
+    //     } else {
+    //       console.log('Message sent: ' + info.response);            
+    //     }; // else statemenet  
+    // }); // transporter.sendMail(mailOptions, function(error, info){
+  } // sendEmail: function(website, email) {
 }
+
+
