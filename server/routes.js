d0ed3527e9e67e5dc5921d0e6e66de1ab6cfd506
John Murphy <chiefsmurph@gmail.com>
2015-07-01 13:07:17 -0700

    [fix] fixes high res

--- server/routes.js
+++ server/routes.js
@@ -34,9 +34,9 @@ var setup = function(app) {
         var url_parts = url.parse(req.url, true);
         var query = url_parts.query;
 
-        basicScraper.getScreenshot(query.url, req.session.user_id, function(imgpath) {
-          console.log('RESULT IMAGE', imgpath);
-          res.send(imgpath);
+        basicScraper.getScreenshot(query.url, req.session.user_id, function(status, path) {
+          console.log('RESULT IMAGE', path);
+          res.send(path);
         });
   });
 
