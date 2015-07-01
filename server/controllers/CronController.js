8ae7da9d468214f55fd17eca1a92d61cec47fa7c
Chris Grovers <chrisgrovers@gmail.com>
2015-06-30 18:58:01 -0700

    [Feature] addCron will check if a userUrl is active before adding a cronjob

--- server/controllers/CronController.js
+++ server/controllers/CronController.js
@@ -30,8 +30,8 @@ module.exports = {
              if (active) {
                console.log('watching ' + url + ' for ' + userUrl.email)
                module.exports.addCron(userUrl, url);
-             }
-          } // for loop iterating over each url for a user
+             }; // if (active)
+          }; // for loop iterating over each url for a user
         }); // .then(function(url){
       }; // or (var i = 0; i < allUsers.length; i++){
     }); // .then(function(allUsers) {
@@ -50,46 +50,33 @@ module.exports = {
     // minutes
     var freq = '* */' + UserUrl.frequency + ' * * * *';
 
-    // TEST every 10 seconds
-    // var freq = '*/10 * * * * *';
-
-    // seconds
-    var freq = '*/' + UserUrl.frequency + ' * * * * *';
+    // FOR TEST PURPOSES ONLY seconds
+    // var freq = '*/' + UserUrl.frequency + ' * * * * *';
 
     if (manager.exists(key)) {
       manager.deleteJob(key);
     };
-
     manager.add(key, freq, function() {
       if (UserUrl.status) {
-
+        console.log('checking', url, 'for', UserUrl.email);
+         var oldImg = UserUrl.cropImage;
+         var email = UserUrl.email;
+         var params = {
+          h: UserUrl.cropHeight,
+          w: UserUrl.cropWidth,
+          x: UserUrl.cropOriginX,
+          y: UserUrl.cropOriginY
+        };
+        // compares screenshot, sends email if there is a difference in image
+        compareUtils.compareScreenShot(UserUrl, url, email, params, oldImg);
       } else {
         manager.stop()
       }
-      console.log('checking', url, 'for', UserUrl.email);
-
-       var oldImg = UserUrl.cropImage;
-       var email = UserUrl.email;
-       var params = {
-        h: UserUrl.cropHeight,
-        w: UserUrl.cropWidth,
-        x: UserUrl.cropOriginX,
-        y: UserUrl.cropOriginY
-      }
-
-      // compares screenshot, sends email if there is a difference in image
-      compareUtils.compareScreenShot(UserUrl, url, email, params, oldImg);
-
     });
-
     manager.start(key);
-
   },
-
   updateCron: function() {
-
   },
-
   startCron: function(user_id, url_id) {
     var key = url_id.toString() + user_id.toString();
     if (manager.exists(key)) {
@@ -99,20 +86,17 @@ module.exports = {
       console.log('error, cronjob', key, ' does not exist');
     }
   },
-
   stopCron: function(user_id, url_id) {
     var key = url_id.toString() + user_id.toString();
     console.log('Stopping cronJob', key);
     manager.stop(key);
   },
-
   deleteCron: function(user_id, url_id) {
     var key = url_id.toString() + user_id.toString();
     console.log('Deleting cronJob', key);
     manager.deleteJob(key);
   },
-
-}
+};
 
 var compareUtils = {
 
@@ -124,24 +108,22 @@ var compareUtils = {
         // checks for difference in pictures
         compare(oldImg, newImg, function (equal){
           if (!equal){
-            
             // set status to false since we are stopping the cronjob
+            console.log('userurl status before', UserUrl.status);
             UserUrl.status = false;
-
+            console.log('userurl status after', UserUrl.status);
             // stop cronjob
             module.exports.stopCron(UserUrl.url_id, UserUrl.user_id)
-
             // if images are not equal, send an email
             compareUtils.sendEmail(website, email);
           }; // if (!equal){
-        }) // compare(img1, img2, function (equal){
+        }); // compare(img1, img2, function (equal){
       }); // basicScraper.cropImg(img1, params, true, function(img2) {
     }, email); // basicScraper.getScreenshot()
   },
 
   sendEmail: function(website, email) {
     console.log('change detected on', website, 'sending email to ', email);
-
     // parameters for email
     var mailOptions = {
         from: "The Blank Arrays <postmaster@sandbox72a87403dd654630bfa3c4b021cda08d.mailgun.org>", // sender address
@@ -150,14 +132,13 @@ var compareUtils = {
         text: 'Hi there! It looks like we found a change on '+ website + '!', // plaintext body
         html: "<span>The Scrapinit team found a change on " + website +"!</span>",
     };
-
     // Send email function
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
             console.log(error);
         }else{
-            console.log('Message sent: ' + info.response);
-        } // else statemenet  
+            console.log('Message sent: ' + info.response);            
+        }; // else statemenet  
     }); // transporter.sendMail(mailOptions, function(error, info){
   }
 
