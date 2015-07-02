c6cc4e25ae9e211227eaba3040ab246d79552dcc
Nayigiziki <josephnayigiziki@gmail.com>
2015-07-02 16:17:26 -0700

    [Feature] added OCR with Tests

--- server/controllers/ocr.js
+++ server/controllers/ocr.js
@@ -1,43 +1,21 @@
 var tesseract = require('node-tesseract');
 
-
-// Recognize text of any language in any format
 var obj = {
-  convertImageToText : function(imagePath, cb){
-
-    tesseract.process('../../client/assets/' + imagePath, function(err, text) {
+  convertImageToText : function(imagePath, cb){ 
+    // sample anonoymous function(stats, text){};
+    tesseract.process(imagePath, function(err, text) {
       if(err) {
-        cb(err, null);
+        cb(err);
       } else {
-        cb('succcess', text);
+        var re = /\n/g;
+        text = text.replace(re, ' ');
+        text = text.trim();
+        console.log('regexd text ' + text);
+        cb(null, text);
       }
     }); 
   },
-  compareOCR : function(imagePath1, imagePath2, cb){
-   //get image of image 1
-   //get image of imgage 2
-   
-   ocr.convertImageToText(imagePath1, function(status1, text1){
-      //if status is not success, text will be null
-      
-      if(status1 === 'success'){
-        ocr.convertImageToText(imagePath2, function(status2, text2){
-          if(status2 === 'success'){
-            cb('success', text1, text2);
-          } else {
-            //status2 failed
-            cb('error', null, null);
-          }
-        });
-      } else {
-        //status1 failed
-        cb('error', null, null);
-      }
-    });
-  }
-
 };
-
 
 
 module.exports = obj;
--- server/specs/ocrSpec.js
+++ server/specs/ocrSpec.js
@@ -1,4 +1,54 @@
-var ocr = require('../controller/ocr.js');
-//regular text
+var ocr = require('../controllers/ocr.js');
+var should = require('should');
+var assert = require('assert');
+var expect = require('expect.js');
 
 
+describe('Image-to-text conversion', function(){
+  var path =  '../../client/assets/1/';
+  it('should return error if invalid path is input', function(done){
+    url = 'fakePath'; 
+    ocr.convertImageToText(path + url, function(error, text){
+      expect(!!error).to.equal(true);
+      done();
+    });
+  });
+
+  it('should not return anything when image, without text, is processed', function(done){ 
+    url = 'www_google_com_fonts.jpg'; 
+    ocr.convertImageToText(path + url, function(error, text){
+     console.log(error);
+     expect(error).to.equal(null);
+     expect(text).to.equal('');
+     done();
+   });
+  });
+
+  it('should not return anything when image, without text, is processed', function(done){ 
+    url = 'www_facebook_com.jpg'; 
+    ocr.convertImageToText(path + url, function(error, text){
+     expect(error).to.equal(null);
+     expect(text).to.equal('Connect with friends and the world around you on Facebook.');
+     done();
+   });
+  });
+
+  it('should text convert an that reads "Open Sans" as such', function(done){ 
+    url = 'www_fontsquirrel_com_fonts_list_popular.jpg'; 
+    ocr.convertImageToText(path + url, function(error, text){
+     expect(error).to.equal(null);
+     expect(text).to.equal('Open Sans');
+     done();
+    });
+  });
+
+  it('should text convert an that reads "Open Sans" as such', function(done){ 
+    url = 'www_google_com.jpg'; 
+    ocr.convertImageToText(path + url, function(error, text){
+     expect(error).to.equal(null);
+     expect(text).to.equal('A faster way to browse the web');
+     done();
+   });
+  });
+
+});
