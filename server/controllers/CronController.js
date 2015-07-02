8a51f7c347699fa61d03e296ac4ae7fbdfd3bdc6
Chris Grovers <chrisgrovers@gmail.com>
2015-07-02 16:04:27 -0700

    [Fix] updated email parameters. Comment out send email function

--- server/controllers/CronController.js
+++ server/controllers/CronController.js
@@ -140,10 +140,8 @@ var compareUtils = {
     var mailOptions = {
         from: "The Blank Arrays <postmaster@sandbox72a87403dd654630bfa3c4b021cda08d.mailgun.org>", // sender address
         to: email, // list of receivers
-        // subject: 'We found some tubular changes!', // Subject line
-        subject: 'Testing images', // Subject line
-        // text: 'Hi there! It looks like we found a change on '+ website + '!', // plaintext body
-        text: "testing",
+        subject: 'We found some tubular changes!', // Subject line
+        text: 'Hi there! It looks like we found a change on '+ website + '!', // plaintext body
         html: "<span>The Scrapinit team found a change on " + website +"!</span>",
         attachments: [
         {
@@ -155,13 +153,13 @@ var compareUtils = {
         ]
     };
     // Send email function
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-          console.log(error);
-        } else {
-          console.log('Message sent: ' + info.response);            
-        }; // else statemenet  
-    }); // transporter.sendMail(mailOptions, function(error, info){
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     if(error){
+    //       console.log(error);
+    //     } else {
+    //       console.log('Message sent: ' + info.response);            
+    //     }; // else statemenet  
+    // }); // transporter.sendMail(mailOptions, function(error, info){
   } // sendEmail: function(website, email) {
 }
 
