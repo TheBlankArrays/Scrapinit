2a68de0d700be5f89c014da8402c3f75df11337f
Dominika <bienkowska.dominika@gmail.com>
2015-07-03 14:40:58 -0700

    [Feature] removed console logs

--- server/controllers/ocr.js
+++ server/controllers/ocr.js
@@ -3,12 +3,8 @@ var gm = require('gm').subClass({ imageMagick: true });
 
 var obj = {
   convertImageToText : function(imagePath, cb){ 
-    console.log('imagePath', imagePath)
     var current = imagePath.split('../');
     var imgPath = '../'+current[2];
-    console.log('imgpath', imgPath)
-    //var filteredImg =imgPath.slice(0,imgPath.length-3)+"tiff";
-    // console.log('fil', filteredImg)
     var imgFil = gm(imgPath).type('grayscale').enhance().unsharp(6.8, 1.0, 2.69, 0).resize(1200,2000).write(imgPath, function (err) {
      if (err){
       console.log('image processing error', err)
