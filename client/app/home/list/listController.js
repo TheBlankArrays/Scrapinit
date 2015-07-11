adce9dd47b2f2fa67b73e83a1752af3c24743116
Nayigiziki <josephnayigiziki@gmail.com>
2015-07-11 11:57:45 -0700

    [Style] updated list dir

--- client/app/home/list/listController.js
+++ client/app/home/list/listController.js
@@ -4,26 +4,23 @@ angular.module('app.home.list', [])
   $scope.currentStep = ipCookie('myBasicTour') || 0;
   // save cookie after each step
   $scope.currentText = undefined;
+  
   $scope.stepComplete = function() {
     ipCookie('myBasicTour', $scope.currentStep, { expires: 3000 });
   };
 
   $scope.getUrls = function () {
-    console.log('in home results');
     Url.getUrls(function (err, urls) {
       if (err) {
         $scope.error = 'We can´t retrieve the URLS';
       }else {
-        console.log('urls are:',url)
         $scope.setUrls(urls);
       }
     });
   };
 
   $scope.remove = function(url) {
-    console.log('remove url: ' + url);
     Url.removeUrl(url, function(success) {
-      console.log('SUCCESS!' + success);
       if (success) {
         $scope.removeUrl(url);
       }
@@ -33,17 +30,13 @@ angular.module('app.home.list', [])
   $scope.getUrls();
 
   $scope.open = function (url) {
-    var str = '<img src="'+ url +'" </img>';
+    var str = '<img src="'+ url +'"</img>';
     ngDialog.open({ 
       template: str,
       className: 'ngdialog-theme-plain',
       plain: true
     });
-  
   };
-
-
-
 })
 .filter('domain', function ($document) {
   return function (input) {
