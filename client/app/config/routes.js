6bf9c5384c0c35df6e4d35702f87186ebbe40542
Nayigiziki <josephnayigiziki@gmail.com>
2015-07-11 15:17:18 -0700

    [Fix] client routes.js

--- client/app/config/routes.js
+++ client/app/config/routes.js
@@ -1,38 +1,49 @@
 /*
- * Routing angular application
-
- * set State, url, templateUrl and controller
+* Routing angular application
+* set State, url, templateUrl and controller
 */
 angular.module('app.routes', ['ui.router'])
 .config(
-  function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise("/welcome");
-    $stateProvider
-      .state('comingSoon', {
-        url: '/comingSoon',
-        templateUrl: 'app/home/comingSoon.html',
-        controller: 'userController'
-      })
-      .state('signup', {
-        url: '/signup',
-        templateUrl: 'app/user/signup.html',
-        controller: 'userController'
-      })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'app/user/login.html',
-        controller: 'userController'
-      })
-      .state('home.urlImage', {
-        url: 'url_image',
-        templateUrl: 'app/home/url_image/url_image.html',
-        controller: 'urlImageController',
-        auth: true
-      })
-      .state('home.list', {
-        url: 'list',
-        templateUrl: 'app/home/list/list.html',
-        controller: 'listController',
-        auth: true
-      });
-  });
+ function($stateProvider, $urlRouterProvider){
+   $urlRouterProvider.otherwise("/login");
+   $stateProvider
+     .state('comingSoon', {
+       url: '/comingSoon',
+       templateUrl: 'app/home/comingSoon.html',
+       controller: 'userController'
+     })
+     .state('welcome', {
+       url: '/welcome',
+       templateUrl: 'app/home/welcome.html',
+       controller: 'userController'
+     })
+     .state('signup', {
+       url: '/signup',
+       templateUrl: 'app/user/signup.html',
+       controller: 'userController'
+     })
+     .state('login', {
+       url: '/login',
+       templateUrl: 'app/user/login.html',
+       controller: 'userController'
+     })
+     .state('home', {
+       url: '/',
+       templateUrl: 'app/home/home.html',
+       controller: 'homeController',
+       auth: true
+     })
+     .state('home.urlImage', {
+       url: 'url_image',
+       templateUrl: 'app/home/url_image/url_image.html',
+       controller: 'urlImageController',
+       auth: true
+     })
+     .state('home.list', {
+       url: 'list',
+       templateUrl: 'app/home/list/list.html',
+       controller: 'listController',
+       auth: true
+     });
+ });
+
