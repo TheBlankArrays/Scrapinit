05146a49b336267ae15995596b4c3e7e8758d16f
John Murphy <chiefsmurph@gmail.com>
2015-07-13 07:27:57 -0700

    [Fix] Missed colon in return of homecontroller

--- client/app/config/routes.js
+++ client/app/config/routes.js
@@ -27,6 +27,12 @@ angular.module('app.routes', ['ui.router'])
         templateUrl: 'app/user/login.html',
         controller: 'userController'
       })
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/home/home.html',
+        controller: 'homeController',
+        auth: true
+      })
       .state('home.urlImage', {
         url: 'url_image',
         templateUrl: 'app/home/url_image/url_image.html',
